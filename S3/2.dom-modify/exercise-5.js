119e4107e953fff751b7f09f3b5d8fa9e28343f5
Alessandrotri <alezuz1988@gmail.com>
2023-03-07 23:14:58 +0100

    act 3

--- S3/2.dom-modify/exercise-5.js
+++ S3/2.dom-modify/exercise-5.js
@@ -1,5 +1,4 @@
-let h2$$ = document.querySelector (['fn-insert-here=']);
+let h2$$ = document.querySelector ('.fn-insert-here');
 
 h2$$.textContent = "'Wubba Lubba dub dub'";
 
-document.h2.appendChild(h2$$);
